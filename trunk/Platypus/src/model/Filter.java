370f6d9b9faf2a94275ec0bd1d86c319f5c0fe18
fredrik.tiwaz@gmail.com <fredrik.tiwaz@gmail.com@ea49e41c-ac89-890d-5f7b-f0b2eb92d780>
2011-04-03 14:10:56 +0000

    Preliminary version of the Filter-interface added

---
+++ trunk/Platypus/src/model/Filter.java
@@ -0,0 +1,19 @@
+package model;
+
+import javax.swing.ImageIcon;
+import java.awt.Component;
+import java.util.HashMap;
+
+public interface Filter {
+
+	public String getName();
+	
+	public String getDescription();
+	
+	public void applyFilter();
+	
+	public ImageIcon getDescriptiveImage();
+	
+	public HashMap<String,Component> getFilterComponents();
+	
+}
