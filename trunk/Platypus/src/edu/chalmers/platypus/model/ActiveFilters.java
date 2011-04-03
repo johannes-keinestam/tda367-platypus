90d3407e9f3a04135ef2ab0ec4c746139e461956
waveisgay@gmail.com <waveisgay@gmail.com@ea49e41c-ac89-890d-5f7b-f0b2eb92d780>
2011-04-03 14:11:08 +0000

    ActiveFilters.java, not tested

---
+++ trunk/Platypus/src/edu/chalmers/platypus/model/ActiveFilters.java
@@ -0,0 +1,33 @@
+package edu.chalmers.platypus.model;
+
+import java.util.ArrayList;
+import java.util.logging.Filter;
+
+public class ActiveFilters {
+	private static ActiveFilters activeFiltersObject;
+	private ArrayList<Filter> activeFilters = new ArrayList<Filter>();
+	
+	private ActiveFilters(){
+
+	}
+	
+	public static  ActiveFilters getActiveFilters(){
+		if (activeFiltersObject == null) {
+			activeFiltersObject = new ActiveFilters();
+		}
+		return activeFiltersObject;
+	}
+	public void addFilter(Filter filter){
+		activeFilters.add(filter);
+	}
+	public ArrayList<Filter> getList(){
+		return activeFilters;
+	}
+	public void removeAll(){
+		activeFilters.clear();
+	}
+	public void loadPreset(ArrayList<Filter> preset){
+		//TODO
+	}
+	
+}
