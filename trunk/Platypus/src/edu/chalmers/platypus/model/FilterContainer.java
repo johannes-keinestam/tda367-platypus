0a04ea45adaea4a35258964cffcba695f4ec0f15
waveisgay@gmail.com <waveisgay@gmail.com@ea49e41c-ac89-890d-5f7b-f0b2eb92d780>
2011-04-03 16:07:04 +0000

    A smaller update.

--- trunk/Platypus/src/edu/chalmers/platypus/model/FilterContainer.java
+++ trunk/Platypus/src/edu/chalmers/platypus/model/FilterContainer.java
@@ -1,6 +1,7 @@
 package edu.chalmers.platypus.model;
 
 import java.io.File;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
@@ -22,35 +23,37 @@ public class FilterContainer {
 	}
 	
 	private void addFilter(File file){
-		
+		//TODO
 	}
 	public Filter getFilter(String name){
-		for (ArrayList<Filter> loadedFilters : f) {
-			if(f.getName.equals(name)){
-				return f;
+		for (Filter filter : loadedFilters) {
+			if(filter.getName().equals(name)){
+				return filter;
 			}
 		}
-			
-		}
+		return null;
+	}
 	
 	public void importFilter(URL[] url){
 		URLClassLoader loader = new URLClassLoader(url); 
-		Class filter;
 		try {
-			filter = loader.loadClass("filter.Filter");
+			Class filter = loader.loadClass("edu.chalmers.platypus.model.FilterClass");
+			try {
+				Filter f = (Filter) filter.newInstance();
+				loadedFilters.add(f);
+				System.out.println(f.getName()+ " loaded");
+			} catch (InstantiationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		} catch (ClassNotFoundException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		try {
-			loadedFilters.add((Filter) filter.newInstance());
-		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
 	}
 	public void scanForFilters(){
 		File folder = new File(System.getenv("USERPROFILE")+"/Platypus/Filters");
@@ -59,7 +62,12 @@ public class FilterContainer {
 	    for (int i = 0; i < listOfFiles.length; i++) {
 	      if (listOfFiles[i].isFile()) {
 	    	URL url[] = new URL[1]; 
-	  		url[0] = new URL("file:///"+System.getenv("USERPROFILE")+"/Platypus/Filters"+listOfFiles[i].getName());
+	  		try {
+				url[0] = new URL("file:///"+System.getenv("USERPROFILE")+"/Platypus/Filters"+listOfFiles[i].getName());
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	  		importFilter(url);
 	      }
 		
