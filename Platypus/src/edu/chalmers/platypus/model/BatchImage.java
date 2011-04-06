4ae2376c080ce1fa0abdca9813706accf88b6154
fredrik.hidstrand@gmail.com <fredrik.hidstrand@gmail.com@ea49e41c-ac89-890d-5f7b-f0b2eb92d780>
2011-04-06 20:56:45 +0000

    First version of BatchImage with basic functionality

--- Platypus/src/edu/chalmers/platypus/model/BatchImage.java
+++ Platypus/src/edu/chalmers/platypus/model/BatchImage.java
@@ -1,19 +1,40 @@
 package edu.chalmers.platypus.model;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 
 public class BatchImage {
-	public BatchImage() {
-		
-	}
-	
-	public ImageIcon getThumbnail() {
-		return new ImageIcon();
-		//produce small imageicon
-	}
-	public String getFileName() {
-		return "";
-		//get file name (blabla.png)
+	private final URI imageURI;
+
+	public BatchImage(final File file) {
+		this.imageURI = file.toURI();
 	}
 
+	public ImageIcon getThumbnail() {
+		try {
+			return new ImageIcon(imageURI.toURL());
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public BufferedImage getImage() {
+		try {
+			return ImageIO.read(imageURI.toURL());
+		} catch (final MalformedURLException e) {		
+			e.printStackTrace();
+			return null;
+		} catch (final IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
+
