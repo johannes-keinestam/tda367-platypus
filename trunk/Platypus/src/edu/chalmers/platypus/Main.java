6321fa1b6ed07ec9ff4907146daaa7ca4deed9e1
johannes.keinestam@gmail.com <johannes.keinestam@gmail.com@ea49e41c-ac89-890d-5f7b-f0b2eb92d780>
2011-04-14 08:28:53 +0000

    Main with dummy filters.

--- trunk/Platypus/src/edu/chalmers/platypus/Main.java
+++ trunk/Platypus/src/edu/chalmers/platypus/Main.java
@@ -1,6 +1,14 @@
 package edu.chalmers.platypus;
 
+import java.awt.Component;
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
 import edu.chalmers.platypus.ctrl.PlatypusCtrl;
+import edu.chalmers.platypus.model.IFilter;
 import edu.chalmers.platypus.model.PlatypusModel;
 import edu.chalmers.platypus.view.PlatypusApp;
 /**
@@ -15,5 +23,74 @@ public class Main {
     	
     	Locator.setModel(model);
     	Locator.setCtrl(ctrl);
+    	
+    	addImgTest();
+    }
+    
+    private static void addImgTest() {
+    	Locator.getCtrl().addFilter(new IFilter() {
+			
+			@Override
+			public String getName() {
+				// TODO Auto-generated method stub
+				return "Filter 1";
+			}
+			
+			@Override
+			public ImageIcon getDescriptiveImage() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public String getDescription() {
+				// TODO Auto-generated method stub
+				return "Filter 1 är awesome är gör asfräcka saker. Jag svär alltså.";
+			}
+			
+			@Override
+			public BufferedImage applyFilter(BufferedImage image) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public JPanel getPanel() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		});
+    	Locator.getCtrl().addFilter(new IFilter() {
+			
+			@Override
+			public String getName() {
+				// TODO Auto-generated method stub
+				return "Filter 2 - blabla";
+			}
+			
+			@Override
+			public ImageIcon getDescriptiveImage() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public String getDescription() {
+				// TODO Auto-generated method stub
+				return "Filter 2 är inte lika bra. Fuckin' shit sucks.";
+			}
+
+			@Override
+			public BufferedImage applyFilter(BufferedImage image) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public JPanel getPanel() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		});
     }
 }
