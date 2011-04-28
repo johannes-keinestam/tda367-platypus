8c0d0d86d69191021a7d8c15085415fb6066111c
waveisgay@gmail.com <waveisgay@gmail.com@ea49e41c-ac89-890d-5f7b-f0b2eb92d780>
2011-04-28 10:27:09 +0000

    New class Preset. Stores info about a preset.

---
+++ trunk/Platypus/src/edu/chalmers/platypus/model/Preset.java
@@ -0,0 +1,32 @@
+package edu.chalmers.platypus.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Preset implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 310479221482604361L;
+	String name;
+	Date date;
+	String filters;
+	
+	public Preset(String name, String filters){
+		this.name = name;
+		this.filters = filters;
+		this.date = new Date();
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public String getFilters() {
+		return filters;
+	}
+}
