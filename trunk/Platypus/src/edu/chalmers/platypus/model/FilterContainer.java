4df983b749a006e99646e980476457475a147f1a
waveisgay@gmail.com <waveisgay@gmail.com@ea49e41c-ac89-890d-5f7b-f0b2eb92d780>
2011-05-12 18:13:16 +0000

    Can now load external libraries to plugins.

--- trunk/Platypus/src/edu/chalmers/platypus/model/FilterContainer.java
+++ trunk/Platypus/src/edu/chalmers/platypus/model/FilterContainer.java
@@ -1,15 +1,23 @@
 package edu.chalmers.platypus.model;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 public class FilterContainer {
 
 	private static FilterContainer filterContainer;
 	private ArrayList<IFilter> loadedFilters = new ArrayList<IFilter>();
+	private URLClassLoader libraryLoader;
 
 	private FilterContainer() {
 		scanForFilters();
@@ -40,7 +48,8 @@ public class FilterContainer {
 	}
 
 	public boolean importFilter(URL[] url) {
-		URLClassLoader loader = new URLClassLoader(url);
+		URLClassLoader loader = new URLClassLoader(url, libraryLoader);
+		
 		try {
 			Class filter = loader
 					.loadClass("edu.chalmers.platypus.model.FilterClass");
@@ -71,13 +80,86 @@ public class FilterContainer {
 				try {
 					url[0] = new URL("file:///" + System.getenv("USERPROFILE")
 							+ "/PlatyPix/Filters/" + listOfFiles[i].getName());
+					scanJarForLibraries(listOfFiles[i].getName());
+					importFilter(url);
 				} catch (MalformedURLException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				importFilter(url);
+				
 			}
 
 		}
+	}
+	private void scanJarForLibraries(String file) {
+		  
+		try {
+			JarFile jarFile = new JarFile(System.getenv("USERPROFILE")
+					+ "/PlatyPix/Filters/"+file);
+			
+			JarEntry entry = jarFile.getJarEntry("Libraries.txt");
+			   BufferedInputStream stream = new BufferedInputStream(jarFile.getInputStream(entry));
+			   loadLibraries(stream);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		catch ( NullPointerException e){
+			
+		}
+
+		
+	}
+	private void loadLibraries(BufferedInputStream stream) {
+	    String thisLine;
+	    try {
+	      BufferedInputStream is =  stream;
+	      BufferedReader br = new BufferedReader
+	         (new InputStreamReader(is));
+	      while ((thisLine = br.readLine()) != null) {  
+	    	  	scanClasses(thisLine);
+	         }
+	      }
+	    catch (Exception e) {
+	      e.printStackTrace();
+	      }
+	  }
+	private void scanClasses(String jar){
+		JarFile jarFile;
+		try {
+			jarFile = new JarFile(System.getenv("USERPROFILE")
+					+ "/PlatyPix/Filters/"+jar);	   
+			   Enumeration<JarEntry> enu = jarFile.entries();
+		       while (enu.hasMoreElements()) {
+		    	   JarEntry h = enu.nextElement();
+		    	   if(!h.isDirectory()&& h.toString().endsWith(".class"))
+		    		   loadClass(h.toString().replaceAll("/", ".").replaceAll(".class", ""), jar);
+		       }
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+	private void loadClass(String file, String jar){
+		URL url[] = new URL[1];
+		try {
+			url[0] = new URL("file:///" + System.getenv("USERPROFILE")
+					+ "/PlatyPix/Filters/"+jar);
+			libraryLoader = new URLClassLoader(url);
+			try {
+				Class cl = libraryLoader.loadClass(file);
+				System.out.println(cl.toString());
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
 }
